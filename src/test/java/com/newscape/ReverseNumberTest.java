ebe2590648a5ba8d898495ca682ab40b5b265ecd
raherwar <144657601+raherwar@users.noreply.github.com>
2023-09-12 11:51:48 +0530

    Add files via upload
    ReverseNumberTest.java file  added for IT.

---
+++ src/test/java/com/newscape/ReverseNumberTest.java
@@ -0,0 +1,22 @@
+package com.newscape;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+
+public class ReverseNumberTest {
+
+	@Test
+	public void testReverse() {
+		
+		int reverse;
+		int number = 321;
+		ReverseNumber num = new ReverseNumber();
+		
+		//reverseNum will reverse the number and store the result in reverse variable.
+		reverse = num.reverseNum(number);
+		
+		//It will check actual result with expected result.
+		assertEquals(125, reverse);
+		
+	}
+}

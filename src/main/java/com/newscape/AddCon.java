faf71d9b4de4dd2a9a9c23b1a79df68f03752c44
agajbhare <144657316+agajbhare@users.noreply.github.com>
2023-09-12 13:47:58 +0530

    Added java file

---
+++ src/main/java/com/newscape/AddCon.java
@@ -0,0 +1,16 @@
+package com.newscape;
+
+public class AddCon {
+	
+	
+	    public int add(int a, int b) {
+	    	//System.out.println(a + b);
+	        return a + b;
+	        
+	    }
+	    
+	    public String concatenate(String str1, String str2) {
+	    	//System.out.println(str1 + str2);
+	        return str1 + str2;
+	    }
+	}
